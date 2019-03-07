a09c626f36ad768dfe437c42e0e33f8b77fd4105
Philippe Lonchampt <philippelonchampt@gmail.com>
2019-03-07 15:50:03 +0100

    L5.8

--- tests/Feature/GumContextTest.php
+++ tests/Feature/GumContextTest.php
@@ -16,7 +16,7 @@ class GumContextTest extends TestCase
 {
     use RefreshDatabase, ContentUrlTestHelpers;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
--- tests/Feature/Sharp/GumSharpTestCase.php
+++ tests/Feature/Sharp/GumSharpTestCase.php
@@ -11,7 +11,7 @@ class GumSharpTestCase extends TestCase
 {
     use RefreshDatabase, SharpAssertions;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
