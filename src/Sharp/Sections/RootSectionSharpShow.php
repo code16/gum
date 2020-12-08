9ea2e59e3c03252cb8742b99678da15cc607c688
billaud <pierre@code16.fr>
2020-12-08 15:52:46 +0100

    Rootsection showpage improved

--- src/Sharp/Sections/RootSectionSharpShow.php
+++ src/Sharp/Sections/RootSectionSharpShow.php
@@ -16,11 +16,25 @@ class RootSectionSharpShow extends SharpShow
     {
         $this->addField(SharpShowTextField::make("title")
             ->setLabel("Titre"))
+            ->addField(SharpShowTextField::make("heading_text")
+                ->setLabel("Chapeau"))
+            ->addField(SharpShowTextField::make("slug")
+                ->setLabel("URL"))
+            ->addField(SharpShowTextField::make("menu_key")
+                ->setLabel("Emplacement"))
             ->addField(
                 SharpShowEntityListField::make("tileblocks", "tileblocks")
                     ->showCreateButton(true)
                     ->setLabel("Tuiles")
                     ->hideFilterWithValue("section", function($instanceId) {
+                        return $instanceId;
+                    })
+            )
+            ->addField(
+                SharpShowEntityListField::make("section_sidepanels", "section_sidepanels")
+                    ->showCreateButton(true)
+                    ->setLabel("Panneaux sections")
+                    ->hideFilterWithValue("container", function($instanceId) {
                         return $instanceId;
                     })
             );
@@ -32,9 +46,16 @@ class RootSectionSharpShow extends SharpShow
             $section
                 ->addColumn(7, function(ShowLayoutColumn $column) {
                     $column->withSingleField("title");
+                })
+                ->addColumn(7, function(ShowLayoutColumn $column) {
+                    $column->withSingleField("heading_text");
+                })
+                ->addColumn(7, function(ShowLayoutColumn $column) {
+                    $column->withFields("slug|3", "menu_key|3");
                 });
             })
-            ->addEntityListSection("tileblocks");
+            ->addEntityListSection("tileblocks")
+            ->addEntityListSection("section_sidepanels");
     }
 
     function find($id): array
