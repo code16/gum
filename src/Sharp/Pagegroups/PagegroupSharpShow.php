dc14bd834a03bd32e42f7cbdb715e8c842792375
billaud <pierre@code16.fr>
2020-12-10 14:54:05 +0100

    WIP improvement for pagegroup show urls

--- src/Sharp/Pagegroups/PagegroupSharpShow.php
+++ src/Sharp/Pagegroups/PagegroupSharpShow.php
@@ -3,6 +3,7 @@
 namespace Code16\Gum\Sharp\Pagegroups;
 
 use Code16\Gum\Models\ContentUrl;
+use Code16\Gum\Models\Page;
 use Code16\Gum\Models\Pagegroup;
 use Code16\Sharp\Http\WithSharpContext;
 use Code16\Sharp\Show\Fields\SharpShowEntityListField;
@@ -20,8 +21,10 @@ class PagegroupSharpShow extends SharpShow
     {
         $this->addField(SharpShowTextField::make("title")
             ->setLabel("Titre"))
-            ->addField(SharpShowTextField::make("slug")
-                ->setLabel("URL"))
+            ->addField(SharpShowTextField::make("url")
+                ->setLabel("URL du groupe"))
+            ->addField(SharpShowTextField::make("urls")
+                ->setLabel("URLs"))
             ->addField(
                 SharpShowEntityListField::make("pages", "pages")
                     ->showCreateButton(true)
@@ -48,7 +51,10 @@ class PagegroupSharpShow extends SharpShow
                     $column->withSingleField("title");
                 })
                 ->addColumn(12, function(ShowLayoutColumn $column) {
-                    $column->withFields("slug|3");
+                    $column->withSingleField("url");
+                })
+                ->addColumn(12, function(ShowLayoutColumn $column) {
+                    $column->withSingleField("urls");
                 });
         })
             ->addEntityListSection("pages");
@@ -56,7 +62,20 @@ class PagegroupSharpShow extends SharpShow
 
     function find($id): array
     {
-        return $this->transform(Pagegroup::find($id));
+        $pagegroup = Pagegroup::find($id);
+
+        return $this
+            ->setCustomTransformer("url", function () use ($pagegroup) {
+                $current = ContentUrl::where('content_id', $pagegroup->id)
+                    ->where('content_type', Pagegroup::class)
+                    ->first();
+
+                return $current ? $current->uri : null;
+            })
+            ->setCustomTransformer("urls", function () use ($pagegroup) {
+                return $this->getPagesUrls($pagegroup);
+            })
+            ->transform($pagegroup);
     }
 
     protected function getParentContentUrl(ContentUrl $contentUrl): ContentUrl
@@ -66,5 +85,26 @@ class PagegroupSharpShow extends SharpShow
         }
 
         return $contentUrl;
+    }
+
+    protected function getPagesUrls(Pagegroup $pagegroup)
+    {
+        $pages = $pagegroup->pages;
+
+        return $pages->map(function ($page) {
+            $contentUrls = ContentUrl::where('content_id', $page->id)
+                ->where('content_type', Page::class)
+                ->get();
+
+            $urls = $contentUrls->map(function ($value) {
+                return $value->uri;
+            })
+                ->flatten()
+                ->implode('<br>');;
+
+            return sprintf("<small>".$page->title."</small> <br>%s",$urls);
+        })
+            ->flatten()
+            ->implode('<br><br>');
     }
 }
