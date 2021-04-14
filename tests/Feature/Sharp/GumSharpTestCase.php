8cdb3d8e74aebf55055d647b38480cc55df11723
pierrebillaud <pierre@code16.fr>
2021-04-14 11:02:02 +0200

    WIP set Sharp tests

--- tests/Feature/Sharp/GumSharpTestCase.php
+++ tests/Feature/Sharp/GumSharpTestCase.php
@@ -2,20 +2,105 @@
 
 namespace Code16\Gum\Tests\Feature\Sharp;
 
+use Code16\Gum\Sharp\Menus\MenuSharpForm;
+use Code16\Gum\Sharp\Menus\MenuSharpList;
+use Code16\Gum\Sharp\News\NewsSharpForm;
+use Code16\Gum\Sharp\News\NewsSharpList;
+use Code16\Gum\Sharp\News\NewsSharpValidator;
+use Code16\Gum\Sharp\Pages\HomepageSharpShow;
+use Code16\Gum\Sharp\Pages\OrphanPageSharpList;
+use Code16\Gum\Sharp\Pages\PageInPagegroupSharpEmbeddedList;
+use Code16\Gum\Sharp\Pages\PageSharpForm;
+use Code16\Gum\Sharp\Pages\PageSharpShow;
+use Code16\Gum\Sharp\Pages\PageSharpValidator;
+use Code16\Gum\Sharp\Sidepanels\SidepanelSharpForm;
+use Code16\Gum\Sharp\Sidepanels\SidepanelSharpList;
+use Code16\Gum\Sharp\Tiles\TileblockSharpForm;
+use Code16\Gum\Sharp\Tiles\TileblockSharpList;
+use Code16\Gum\Sharp\Tiles\TileblockSharpValidator;
 use Code16\Gum\Tests\Feature\Utils\UserModel;
 use Code16\Gum\Tests\TestCase;
 use Code16\Sharp\Utils\Testing\SharpAssertions;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Faker\Generator;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Str;
 
 class GumSharpTestCase extends TestCase
 {
-    use RefreshDatabase, SharpAssertions;
+    use SharpAssertions;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->initSharpAssertions();
-        $this->loginAsSharpUser(factory(UserModel::class)->create());
+        $this->loginAsSharpUser(\factory(UserModel::class)->create());
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        $app['config']->set([
+            "sharp" => [
+                "entities" => [
+                    "homepage" => [
+                        "show" => HomepageSharpShow::class
+                    ],
+                    "menus" => [
+                        "form" => MenuSharpForm::class,
+                        "list" => MenuSharpList::class
+                    ],
+                    "news" => [
+                        "list" => NewsSharpList::class,
+                        "form" => NewsSharpForm::class,
+                        "validator" => NewsSharpValidator::class,
+                    ],
+                    "orphan_pages" => [
+                        "form" => PageSharpForm::class,
+                        "list" => OrphanPageSharpList::class,
+                        "show" => PageSharpShow::class,
+                        "validator" => PageSharpValidator::class,
+                    ],
+                    "sidepanels" => [
+                        "list" => SidepanelSharpList::class,
+                        "form" => SidepanelSharpForm::class
+                    ],
+                    "pages" => [
+                        "form" => PageSharpForm::class,
+                        "list" => PageInPagegroupSharpEmbeddedList::class,
+                        "show" => PageSharpShow::class,
+                        "validator" => PageSharpValidator::class,
+                    ],
+                    "tileblocks" => [
+                        "list" => TileblockSharpList::class,
+                        "form" => TileblockSharpForm::class,
+                        "validator" => TileblockSharpValidator::class
+                    ]
+                ]
+            ]
+        ]);
+
+        app(Factory::class)->define(UserModel::class, function (Generator $faker) {
+            return [
+                'email' => $faker->unique()->safeEmail,
+                'name' => $faker->name,
+                'password' => bcrypt('secret'),
+                'remember_token' => Str::random(10),
+            ];
+        });
     }
 }
--- tests/Feature/Sharp/NewsSharpTest.php
+++ tests/Feature/Sharp/NewsSharpTest.php
@@ -2,9 +2,6 @@
 
 namespace Code16\Gum\Tests\Feature\Sharp;
 
-use Carbon\Carbon;
-use Code16\Gum\Models\News;
-use Code16\Gum\Tests\TestCase;
 use Code16\Sharp\Utils\Testing\SharpAssertions;
 
 class NewsSharpTest extends GumSharpTestCase
@@ -12,24 +9,14 @@ class NewsSharpTest extends GumSharpTestCase
     use SharpAssertions;
 
     /** @test */
-    function we_can_create_news()
+    function we_can_update_news()
     {
-        Carbon::setTestNow();
-
-        $news = factory(News::class)->make();
-
-        $this
-            ->getSharpForm("news", $news->id)
-            ->assertOk();
-
-        $this
-            ->storeSharpForm("news", [
-                "title" => "Titre"
-            ])
-            ->assertOk();
+        $this->storeSharpForm("news", [
+            "title" => "Title"
+        ]);
 
         $this->assertDatabaseHas("news", [
-            "title" => "Titre"
+            "title" => "Title"
         ]);
     }
 }
